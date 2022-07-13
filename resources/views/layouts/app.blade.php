1e1fc5a49c486c113da4b580ada20c39f6c7c617
Ijal Fauzi <ijaliffauzi@gmail.com>
2022-07-13 16:52:37 +0700

    Changed logout text.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -198,7 +198,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
             <a href="/logout" class="nav-link">
               <i class="nav-icon fas fa-power-off"></i>
               <p>
-                Logout
+                Keluar
               </p>
             </a>
           </li>
